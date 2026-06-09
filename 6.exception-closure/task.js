207c5dc92adeeaa8a2b309394695722669d2b4ee
Netology <yana.shishlina03.07.19@mail.ru>
2026-06-09 09:43:17 +0500

    Задача 6

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,48 @@
-﻿
+﻿function parseCount(value) {
+    const parsed = Number.parseFloat (value);
+    if (isNaN(parsed)) {
+        throw new Error("Невалидное значение")
+    } return parsed;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor (a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    } 
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const floor_perimeter = this.perimeter / 2;
+        const area = Math.sqrt(floor_perimeter * (floor_perimeter - this.a) * (floor_perimeter - this.b) * (floor_perimeter - this.c));
+        return Math.round(area * 1000) / 1000;
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+    }
+}
